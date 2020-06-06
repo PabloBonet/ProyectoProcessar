ea468140ce7413f221a6115d0a284e5b8d30ae3e
tuliorossi <tuliorossi@yahoo.com.ar>
2020-06-06 19:31:15 -0300

    REGISTRAR
    REGISTRACION DE LAS DLL

---
+++ registradll.bat
@@ -0,0 +1,5 @@
+cd c:\
+cd c:\Proyectos\processar
+C:\WINDOWS\microsoft.net\framework\v4.0.30319\regasm c:\Proyectos\processar\ModuloAfip.dll  /register /codebase   /tlb
+C:\WINDOWS\microsoft.net\framework\v4.0.30319\regasm c:\Proyectos\processar\EntidadesClass.dll /register /codebase /tlb 
+pause 
