93c9bb06681c905df77d4de67319413fbee59e25
Tulio -Rossi <tuliorossi@yahoo.com.ar>
2022-07-28 21:18:25 -0300

    OTPresupu
    otpresupu

---
+++ BackupBD/otpresupu_20220728 2117.sql
@@ -0,0 +1,54 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.5.55-0+deb8u1
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema processarmkfc
+--
+
+CREATE DATABASE IF NOT EXISTS processarmkfc;
+USE processarmkfc;
+
+--
+-- Definition of table `otpresupu`
+--
+
+DROP TABLE IF EXISTS `otpresupu`;
+CREATE TABLE `otpresupu` (
+  `idotpresu` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idot` int(10) unsigned NOT NULL,
+  `idpresupu` int(10) unsigned NOT NULL,
+  `fecha` char(8) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idotpresu`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `otpresupu`
+--
+
+/*!40000 ALTER TABLE `otpresupu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `otpresupu` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
