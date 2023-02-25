bb667d0810e87f9e74d94cc1fe21fa0a81ebbf79
PabloBonet <pablo94_88@hotmail.com>
2023-02-25 12:15:17 -0300

    Actualización
    Agregado de formulario y tabla para manejo de indices de inflación

---
+++ BackupBD/indiceInf_20230225 1214.sql
@@ -0,0 +1,57 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.5.55-0+deb8u1
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema trsoftdb
+--
+
+CREATE DATABASE IF NOT EXISTS trsoftdb;
+USE trsoftdb;
+
+--
+-- Definition of table `indiceinf`
+--
+
+DROP TABLE IF EXISTS `indiceinf`;
+CREATE TABLE `indiceinf` (
+  `idindinf` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `periodo` char(6) NOT NULL,
+  `indice` double(13,9) NOT NULL,
+  `observa` char(250) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idindinf`),
+  KEY `periodo` (`periodo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `indiceinf`
+--
+
+/*!40000 ALTER TABLE `indiceinf` DISABLE KEYS */;
+INSERT INTO `indiceinf` (`idindinf`,`periodo`,`indice`,`observa`,`timestamp`) VALUES 
+ (0,'202305',1.123456789,'ASDA','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `indiceinf` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
